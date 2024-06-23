abe503b5722dcb3b853b65b0f06042735c74d853
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-06-23 11:07:04 +0100

    Sets up functionality for pages and begins registering page blocks.

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-const port = 5173;
-const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
+import { defineConfig } from 'vite'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Filament/**',
+                'app/Forms/Components/**',
+                'app/Livewire/**',
+                'app/Infolists/Components/**',
+                'app/Providers/Filament/**',
+                'app/Tables/Columns/**',
+            ],
         }),
     ],
-    server: {
-        // respond to all network requests
-        host: '0.0.0.0',
-        port: port,
-        strictPort: true,
-        // Defines the origin of the generated asset URLs during development,
-        // this will also be used for the public/hot file (Vite devserver URL)
-        origin: origin
-    }
-});
+})
