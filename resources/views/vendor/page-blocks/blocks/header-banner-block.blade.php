2b8fa70f5802f7633aec3c10d5cffa65316ffd15
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-07-16 16:59:17 +0100

    Tweak header banner to match current site and finish up the blog article page.

--- resources/views/vendor/page-blocks/blocks/header-banner-block.blade.php
+++ resources/views/vendor/page-blocks/blocks/header-banner-block.blade.php
@@ -2,10 +2,12 @@
     <div class="top-0 w-full">
         <div class="w-full mb-4 box-border bg-cover bg-center" @if(!empty($image_url)) style="background-image: url('{{ $image_url }}')" @endif>
             <div class="max-w-screen-xl mx-auto flex">
-                <div class="bg-white text-left p-4 mt-40 rounded-xl mx-4 md:mx-auto lg:mx-4 xl:mx-0 mb-16 md:mb-12">
+                <div class="w-full lg:w-1/2 mx-4 xl:mx-0 bg-white text-left p-8 mt-40 md:mb-4 rounded-xl">
                     <h2 class="font-bold text-xl md:text-3xl">{{ $title }}</h2>
                     <p class="font-bold">{{ $subtitle }}</p>
-                    <a class="mt-4 text-white button p-2 inline-block rounded hover:font-normal" href="{{ $link_url }}">{{ $link_text }}</a>
+                    @if (!empty($link_url) && !empty($link_text))
+                        <a class="mt-4 text-white button p-2 inline-block rounded hover:font-normal" href="{{ $link_url }}">{{ $link_text }}</a>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/vendor/posts-module/post.blade.php
+++ resources/views/vendor/posts-module/post.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="bg-white p-8 shadow-xl rounded-xl w-full lg:w-8/12">
         <div class="page-content">
             {!! Str::markdown($post->content) !!}
@@ -13,28 +12,24 @@
     'title' => $post->title,
     'image_url' => $post->featuredImage->url,
     'subtitle' => $post->excerpt,
-    'link_url' => route('posts.show', $post),
-    'link_text' => 'Read More',
 ])
 
 @section('sidebar')
     <div class="bg-white p-8 shadow-xl rounded-xl">
         <div class="flex gap-2">
-            <div>
-                {{-- {{ author.avatar }}
-                    <a href="/" class="cursor-pointer"><img class="rounded-full" src="{{ glide src="{{ url }}" width="100" }}" alt="{{ alt }}" /></a>
-                {{ /author.avatar }} --}}
-            </div>
+            <div><img class="rounded-full" src="https://ui-avatars.com/api/?name={{$post->author?->name}}&length=1&rounded=true&size=100" /></div>
             <div class="self-center">
-                <p><span class="font-bold">Posted By:</span> {{ 'author.name' }}</p>
-                <p><span class="font-bold">On:</span> {{ 'date' }}</p>
+                <p><span class="font-bold">Posted By:</span> {{ $post->author?->name }}</p>
+                <p><span class="font-bold">On:</span> {{ $post->published_at->format('d/m/Y') }}</p>
             </div>
         </div>
         <hr class="my-4" />
         <div class="mt-4 flex items-center">
             <p><span class="mr-2 font-bold">Tags:</span></p>
             <div class="flex grid-cols-2 gap-2 flex-wrap justify-end">
-            Categories...
+                @foreach ($post->tags as $tag)
+                    <a class="button" href="">{{ $tag->name }}</a>
+                @endforeach
             </div>
         </div>
     </div>
