b1124c1ad75feb5788ab08cd06a6c02f2dd046b7
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-07-16 17:00:29 +0100

    Fix category block markup.

--- resources/views/vendor/page-blocks/blocks/categories-block.blade.php
+++ resources/views/vendor/page-blocks/blocks/categories-block.blade.php
@@ -3,7 +3,7 @@
         <h2 class="text-xl mb-4 text-fuchsia-pink">{{ $title }}</h2>
         @foreach ($categories as $category)
             <span class="inline-block py-2">
-                <a class="button " href="/categories/craft-cms">{{ $category->name }} <span class="font-bold text-white">({{ $category->posts_count ?? 0}})</span></a>
+                <a class="button " href="/categories/{{ $category->slug }}">{{ $category->name }} <span class="font-bold text-white">({{ $category->posts_count ?? 0}})</span></a>
             </span>
         @endforeach
     </div>
